ca426d87e95a4adc5b0e116f83cac5d9ffbfc9e0
zpendlet <atypicalmc@gmail.com>
2024-02-22 14:52:00 -0500

    finished?

---
+++ QueryBuilder/QueryBuilder/Models/IClassModel.cs
@@ -0,0 +1,10 @@
+﻿namespace QueryBuilderProject
+{
+    /// <summary>
+    /// This enforces objects read in by QueryBuilder to have an ID, which reflects in the database as a PKey
+    /// </summary>
+    public interface IClassModel
+    {
+        public int Id { get; set; }
+    }
+}
